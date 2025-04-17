28ded7874294dc1bbaeb7373e2d8a713320eb161
Alsu Fakhrutdinova <alsou9305@ya.ru>
2025-04-17 23:28:26 +0500

    locator

--- src/main/java/ru/praktikum/ListCheck.java
+++ src/main/java/ru/praktikum/ListCheck.java
@@ -11,28 +11,35 @@ import java.time.Duration;
 
 public class ListCheck {
     private WebDriver driver;
+    public By cookieButton = By.className("App_CookieButton__3cvqF");
 
-public ListCheck (WebDriver driver) {
+    public ListCheck (WebDriver driver) {
         this.driver = driver;
     }
 
     public void clickAccordion(int index) {
-        String accordionHeadingId = "accordion__heading-"+index;
-        WebElement accordionHeadingElement = driver.findElement(By.id(accordionHeadingId));
+        WebElement accordionHeadingElement = driver.findElement(getByAccordionHeading(index));
         ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", accordionHeadingElement);
-        WebElement element = new WebDriverWait(driver,Duration.ofSeconds(5)).until(ExpectedConditions.elementToBeClickable(By.id(accordionHeadingId)));
+        WebElement element = new WebDriverWait(driver,Duration.ofSeconds(5)).until(ExpectedConditions.elementToBeClickable(accordionHeadingElement));
         element.click();
     }
 
     public String getPanelText(int index) {
-        String accordionPanelId = "//div[@id='accordion__panel-"+index+"']/p";
-        By text = By.xpath(accordionPanelId);
+        By text = getByAccordionPanel(index);
         WebElement element = new WebDriverWait(driver,Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfElementLocated(text));
         return element.getText();
     }
 
+    private By getByAccordionPanel(int index) {
+        return By.xpath("//div[@id='accordion__panel-"+index+"']/p");
+    }
+
+    private By getByAccordionHeading(int index) {
+        return By.id("accordion__heading-"+index);
+    }
+
     public void closeCookie() {
-        driver.findElement(By.className("App_CookieButton__3cvqF")).click();
+        driver.findElement(cookieButton).click();
     }
 
     public void openPage() {
